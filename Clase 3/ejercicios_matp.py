9410d64e07c471aeb878b8f2a7ffeeefc970c946
NahuMeza <nahuelgmeza@gmail.com>
2025-06-25 10:06:36 -0300

    add: third lesson excersices

---
+++ Clase 3/ejercicios_matp.py
@@ -0,0 +1,19 @@
+# Ejercicio 1 – Gráfico de Líneas básico
+# Crea un gráfico de líneas que muestre la evolución de la temperatura durante 5 días.
+
+# Días: 1, 2, 3, 4, 5
+
+# Temperaturas: 22, 24, 20, 23, 25
+
+# Agrega título y etiquetas a los ejes.
+
+
+
+# Ejercicio 2 – Gráfico de Barras
+# Muestra un gráfico de barras con la cantidad de ventas de tres productos:
+
+# Producto A: 50 ventas
+
+# Producto B: 80 ventas
+
+# Producto C: 30 ventas
---
+++ Clase 3/ejercicios_pd.py
@@ -0,0 +1,20 @@
+# Ejercicio 1 – Crear un DataFrame
+# Crea un DataFrame con la siguiente información de personas:
+
+# Nombre	Edad	Ciudad
+# Ana	23	Madrid
+# Luis	34	Barcelona
+# Sofía	29	Valencia
+
+# Mostrá el DataFrame completo.
+
+# Ejercicio 2 – Agregar una columna
+# Agregá una nueva columna llamada Salario con los valores: 2000, 2500, 2200.
+
+# Mostrá el DataFrame actualizado.
+
+# Ejercicio 4 – Ordenar por Salario
+# Ordená el DataFrame por la columna Salario de mayor a menor.
+
+# Ejercicio 6 - Guardar en CSV
+# Guarda el DataFrame resultante en un archivo csv llamado resultado.csv
